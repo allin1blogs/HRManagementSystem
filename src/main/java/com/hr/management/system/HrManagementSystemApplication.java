8a1e7c39304a89b885e339b035348fbff9777c36
Avnish <sainiavnishkumar@gmail.com>
2020-08-06 12:34:13 +0530

    Testing.....

---
+++ src/main/java/com/hr/management/system/HrManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.hr.management.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrManagementSystemApplication.class, args);
+		System.out.println("=======Started HrManagementSystemApplication===============");
+	}
+
+}
